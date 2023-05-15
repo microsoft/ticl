7ccd4cf36a702bf7dc2ddcc5ffd7d13c59fa4ad4
amueller <amueller@microsoft.com>
2023-05-15 23:39:32 +0000

    remove unused for-loop
    Former-commit-id: ff9ed3275cce80b18206e46b70207dbb7d9427ad

--- tabpfn/scripts/baseline_prediction_interface.py
+++ tabpfn/scripts/baseline_prediction_interface.py
@@ -19,21 +19,18 @@ def baseline_predict(metric_function, eval_xs, eval_ys, categorical_feats, metri
     outputs = []
     best_configs = []
     eval_splits = list(zip(eval_xs.transpose(0, 1), eval_ys.transpose(0, 1)))
-    for eval_x, eval_y in tqdm.tqdm(eval_splits, desc='Calculating splits'+str(metric_function)+' '+str(eval_pos)):
-        #try:
-        if True:
-            metric, output, best_config = metric_function(eval_x[:eval_pos],
-                                                          eval_y[:eval_pos],
-                                                          eval_x[eval_pos:],
-                                                          eval_y[eval_pos:],
-                                                          categorical_feats,
-                                                          metric_used=metric_used
-                                                          , max_time=max_time)
-            metrics += [metric]
-            outputs += [output]
-            best_configs += [best_config]
-            return np.array(metrics), np.array(outputs), best_configs
-        # except Exception as e:
-        #    print(f'There was an exception in {metric_function}')
-        #    print(e)
-        #    return None, None, None
+    if len(eval_splits) > 1:
+        import pdb; pdb.set_trace()
+    (eval_x, eval_y), = eval_splits
+
+    metric, output, best_config = metric_function(eval_x[:eval_pos],
+                                                    eval_y[:eval_pos],
+                                                    eval_x[eval_pos:],
+                                                    eval_y[eval_pos:],
+                                                    categorical_feats,
+                                                    metric_used=metric_used
+                                                    , max_time=max_time)
+    metrics += [metric]
+    outputs += [output]
+    best_configs += [best_config]
+    return np.array(metrics), np.array(outputs), best_configs
