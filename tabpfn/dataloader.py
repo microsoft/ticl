ab87d0eb33b59dfe76f358a1999ceaa998c709be
Andreas C Mueller <amueller@microsoft.com>
2023-05-26 20:56:11 +0000

    more dataloader refactor
    Former-commit-id: 80fc634ee54385185cf087209932cdb78c48942a

--- tabpfn/dataloader.py
+++ tabpfn/dataloader.py
@@ -1,6 +1,110 @@
+import tabpfn.priors as priors
+
+def get_mlp_prior_hyperparameters(config):
+    from tabpfn.priors.utils import gamma_sampler_f
+    config = {hp: (list(config[hp].values())[0]) if type(config[hp]) is dict else config[hp] for hp in config}
+
+    if 'random_feature_rotation' not in config:
+        config['random_feature_rotation'] = True
+
+    if "prior_sigma_gamma_k" in config:
+        sigma_sampler = gamma_sampler_f(config["prior_sigma_gamma_k"], config["prior_sigma_gamma_theta"])
+        config['init_std'] = sigma_sampler
+    if "prior_noise_std_gamma_k" in config:
+        noise_std_sampler = gamma_sampler_f(config["prior_noise_std_gamma_k"], config["prior_noise_std_gamma_theta"])
+        config['noise_std'] = noise_std_sampler
+
+    return config
 
 
-def get_dataloader(priordataloader_class, steps_per_epoch, batch_size, single_eval_pos_gen, bptt, bptt_extra_samples, device, **extra_prior_kwargs_dict):
+def get_gp_mix_prior_hyperparameters(config):
+    return {'lengthscale_concentration': config["prior_lengthscale_concentration"],
+            'nu': config["prior_nu"],
+            'outputscale_concentration': config["prior_outputscale_concentration"],
+            'categorical_data': config["prior_y_minmax_norm"],
+            'y_minmax_norm': config["prior_lengthscale_concentration"],
+            'noise_concentration': config["prior_noise_concentration"],
+            'noise_rate': config["prior_noise_rate"]}
+
+def get_gp_prior_hyperparameters(config):
+    return {hp: (list(config[hp].values())[0]) if type(config[hp]) is dict else config[hp] for hp in config}
+
+
+def make_get_batch(model_proto, **extra_kwargs):
+    def new_get_batch(batch_size, seq_len, num_features, hyperparameters
+            , device, model_proto=model_proto
+            , **kwargs):
+        kwargs = {**extra_kwargs, **kwargs} # new args overwrite pre-specified args
+        return model_proto.get_batch(
+            batch_size=batch_size
+            , seq_len=seq_len
+            , device=device
+            , hyperparameters=hyperparameters
+            , num_features=num_features, **kwargs)
+    return new_get_batch
+
+
+def get_dataloader(prior_type, flexible, differentiable, config, steps_per_epoch, batch_size,
+                   single_eval_pos_gen, bptt, bptt_extra_samples, device):
+
+
+    extra_kwargs = {}
+
+    if prior_type == 'prior_bag':
+        # Prior bag combines priors
+        get_batch_gp = make_get_batch(priors.fast_gp)
+        get_batch_mlp = make_get_batch(priors.mlp)
+        if flexible:
+            get_batch_gp = make_get_batch(priors.flexible_categorical, **{'get_batch': get_batch_gp})
+            get_batch_mlp = make_get_batch(priors.flexible_categorical, **{'get_batch': get_batch_mlp})
+        prior_bag_hyperparameters = {'prior_bag_get_batch': (get_batch_gp, get_batch_mlp)
+            , 'prior_bag_exp_weights_1': 2.0}
+        prior_hyperparameters = {**get_mlp_prior_hyperparameters(config), **get_gp_prior_hyperparameters(config)
+            , **prior_bag_hyperparameters}
+        model_proto = priors.prior_bag
+    else:
+        if prior_type == 'mlp':
+            prior_hyperparameters = get_mlp_prior_hyperparameters(config)
+            model_proto = priors.mlp
+        elif prior_type == 'gp':
+            prior_hyperparameters = get_gp_prior_hyperparameters(config)
+            model_proto = priors.fast_gp
+        elif prior_type == 'gp_mix':
+            prior_hyperparameters = get_gp_mix_prior_hyperparameters(config)
+            model_proto = priors.fast_gp_mix
+        else:
+            raise Exception()
+
+        if flexible:
+            get_batch_base = make_get_batch(model_proto)
+            extra_kwargs['get_batch'] = get_batch_base
+            model_proto = priors.flexible_categorical
+
+    if flexible:
+        prior_hyperparameters['normalize_labels'] = True
+        prior_hyperparameters['check_is_compatible'] = True
+    prior_hyperparameters['prior_mlp_scale_weights_sqrt'] = config['prior_mlp_scale_weights_sqrt'] if 'prior_mlp_scale_weights_sqrt' in prior_hyperparameters else None
+    prior_hyperparameters['rotate_normalized_labels'] = config['rotate_normalized_labels'] if 'rotate_normalized_labels' in prior_hyperparameters else True
+
+    use_style = False
+
+    if differentiable:
+        get_batch_base = make_get_batch(model_proto, **extra_kwargs)
+        extra_kwargs = {'get_batch': get_batch_base, 'differentiable_hyperparameters': config['differentiable_hyperparameters']}
+        model_proto = priors.differentiable_prior
+        use_style = True
+    print(f"Using style prior: {use_style}")
+
+
+    extra_prior_kwargs_dict={
+                'num_features': config['num_features']
+                , 'hyperparameters': prior_hyperparameters
+                #, 'dynamic_batch_size': 1 if ('num_global_att_tokens' in config and config['num_global_att_tokens']) else 2
+                , 'batch_size_per_gp_sample': config.get('batch_size_per_gp_sample', None)
+                , **extra_kwargs
+            }
+    
+
     single_eval_pos_gen = single_eval_pos_gen if callable(single_eval_pos_gen) else lambda: single_eval_pos_gen
 
     def eval_pos_seq_len_sampler():
@@ -10,5 +114,5 @@ def get_dataloader(priordataloader_class, steps_per_epoch, batch_size, single_ev
         else:
             return single_eval_pos, bptt
 
-    return priordataloader_class(num_steps=steps_per_epoch, batch_size=batch_size, eval_pos_seq_len_sampler=eval_pos_seq_len_sampler,
+    return model_proto.DataLoader(num_steps=steps_per_epoch, batch_size=batch_size, eval_pos_seq_len_sampler=eval_pos_seq_len_sampler,
                                  seq_len_maximum=bptt+(bptt_extra_samples if bptt_extra_samples else 0), device=device, **extra_prior_kwargs_dict)
--- tabpfn/scripts/model_builder.py
+++ tabpfn/scripts/model_builder.py
@@ -7,8 +7,9 @@ from tabpfn.dataloader import get_dataloader
 from tabpfn.assemble_model import assemble_model
 
 import torch
-from torch import nn
 
+import subprocess as sp
+import os
 import math
 
 def save_model(model, path, filename, config_sample):
@@ -34,8 +35,6 @@ def save_model(model, path, filename, config_sample):
     torch.save((model.state_dict(), None, config_sample), os.path.join(path, filename))
 
 
-import subprocess as sp
-import os
 
 def get_gpu_memory():
     command = "nvidia-smi"
@@ -144,80 +143,10 @@ def load_model(path, filename, device, eval_positions, verbose):
 
     return model, config_sample
 
-def fix_loaded_config_sample(loaded_config_sample, config):
-    def copy_to_sample(*k):
-        t,s = loaded_config_sample, config
-        for k_ in k[:-1]:
-            t = t[k_]
-            s = s[k_]
-        t[k[-1]] = s[k[-1]]
-    copy_to_sample('num_features_used')
-    copy_to_sample('num_classes')
-    copy_to_sample('differentiable_hyperparameters','prior_mlp_activations','choice_values')
-
-def load_config_sample(path, template_config):
-    model_state, optimizer_state, loaded_config_sample = torch.load(path, map_location='cpu')
-    fix_loaded_config_sample(loaded_config_sample, template_config)
-    return loaded_config_sample
-
-def get_default_spec(test_datasets, valid_datasets):
-    bptt = 10000
-    eval_positions = [1000, 2000, 3000, 4000, 5000] # list(2 ** np.array([4, 5, 6, 7, 8, 9, 10, 11, 12]))
-    max_features = max([X.shape[1] for (_, X, _, _, _, _) in test_datasets] + [X.shape[1] for (_, X, _, _, _, _) in valid_datasets])
-    max_splits = 5
-
-    return bptt, eval_positions, max_features, max_splits
-
-def get_mlp_prior_hyperparameters(config):
-    from tabpfn.priors.utils import gamma_sampler_f
-    config = {hp: (list(config[hp].values())[0]) if type(config[hp]) is dict else config[hp] for hp in config}
-
-    if 'random_feature_rotation' not in config:
-        config['random_feature_rotation'] = True
-
-    if "prior_sigma_gamma_k" in config:
-        sigma_sampler = gamma_sampler_f(config["prior_sigma_gamma_k"], config["prior_sigma_gamma_theta"])
-        config['init_std'] = sigma_sampler
-    if "prior_noise_std_gamma_k" in config:
-        noise_std_sampler = gamma_sampler_f(config["prior_noise_std_gamma_k"], config["prior_noise_std_gamma_theta"])
-        config['noise_std'] = noise_std_sampler
-
-    return config
-
-
-def get_gp_mix_prior_hyperparameters(config):
-    return {'lengthscale_concentration': config["prior_lengthscale_concentration"],
-            'nu': config["prior_nu"],
-            'outputscale_concentration': config["prior_outputscale_concentration"],
-            'categorical_data': config["prior_y_minmax_norm"],
-            'y_minmax_norm': config["prior_lengthscale_concentration"],
-            'noise_concentration': config["prior_noise_concentration"],
-            'noise_rate': config["prior_noise_rate"]}
-
-def get_gp_prior_hyperparameters(config):
-    return {hp: (list(config[hp].values())[0]) if type(config[hp]) is dict else config[hp] for hp in config}
-
-
-def get_meta_gp_prior_hyperparameters(config):
-    from tabpfn.priors.utils import trunc_norm_sampler_f
-    config = {hp: (list(config[hp].values())[0]) if type(config[hp]) is dict else config[hp] for hp in config}
-
-    if "outputscale_mean" in config:
-        outputscale_sampler = trunc_norm_sampler_f(config["outputscale_mean"]
-                                                   , config["outputscale_mean"] * config["outputscale_std_f"])
-        config['outputscale'] = outputscale_sampler
-    if "lengthscale_mean" in config:
-        lengthscale_sampler = trunc_norm_sampler_f(config["lengthscale_mean"],
-                                                   config["lengthscale_mean"] * config["lengthscale_std_f"])
-        config['lengthscale'] = lengthscale_sampler
-
-    return config
-
 
 def get_model(config, device, should_train=True, verbose=False, state_dict=None, epoch_callback=None, load_model_strict=True):
     import tabpfn.priors as priors
     from tabpfn.train import train, Losses
-    extra_kwargs = {}
     verbose_train, verbose_prior = verbose >= 1, verbose >= 2
     config['verbose'] = verbose_prior
 
@@ -228,65 +157,8 @@ def get_model(config, device, should_train=True, verbose=False, state_dict=None,
     config['batch_size'] = math.ceil(config['batch_size'] / config['aggregate_k_gradients'])
     config['recompute_attn'] = config['recompute_attn'] if 'recompute_attn' in config else False
 
-    def make_get_batch(model_proto, **extra_kwargs):
-        def new_get_batch(batch_size, seq_len, num_features, hyperparameters
-                , device, model_proto=model_proto
-                , **kwargs):
-            kwargs = {**extra_kwargs, **kwargs} # new args overwrite pre-specified args
-            return model_proto.get_batch(
-                batch_size=batch_size
-                , seq_len=seq_len
-                , device=device
-                , hyperparameters=hyperparameters
-                , num_features=num_features, **kwargs)
-        return new_get_batch
 
-    if config['prior_type'] == 'prior_bag':
-        # Prior bag combines priors
-        get_batch_gp = make_get_batch(priors.fast_gp)
-        get_batch_mlp = make_get_batch(priors.mlp)
-        if 'flexible' in config and config['flexible']:
-            get_batch_gp = make_get_batch(priors.flexible_categorical, **{'get_batch': get_batch_gp})
-            get_batch_mlp = make_get_batch(priors.flexible_categorical, **{'get_batch': get_batch_mlp})
-        prior_bag_hyperparameters = {'prior_bag_get_batch': (get_batch_gp, get_batch_mlp)
-            , 'prior_bag_exp_weights_1': 2.0}
-        prior_hyperparameters = {**get_mlp_prior_hyperparameters(config), **get_gp_prior_hyperparameters(config)
-            , **prior_bag_hyperparameters}
-        model_proto = priors.prior_bag
-    else:
-        if config['prior_type'] == 'mlp':
-            prior_hyperparameters = get_mlp_prior_hyperparameters(config)
-            model_proto = priors.mlp
-        elif config['prior_type'] == 'gp':
-            prior_hyperparameters = get_gp_prior_hyperparameters(config)
-            model_proto = priors.fast_gp
-        elif config['prior_type'] == 'gp_mix':
-            prior_hyperparameters = get_gp_mix_prior_hyperparameters(config)
-            model_proto = priors.fast_gp_mix
-        else:
-            raise Exception()
-
-        if 'flexible' in config and config['flexible']:
-            get_batch_base = make_get_batch(model_proto)
-            extra_kwargs['get_batch'] = get_batch_base
-            model_proto = priors.flexible_categorical
-
-    if config.get('flexible'):
-        prior_hyperparameters['normalize_labels'] = True
-        prior_hyperparameters['check_is_compatible'] = True
-    prior_hyperparameters['prior_mlp_scale_weights_sqrt'] = config[
-        'prior_mlp_scale_weights_sqrt'] if 'prior_mlp_scale_weights_sqrt' in prior_hyperparameters else None
-    prior_hyperparameters['rotate_normalized_labels'] = config[
-        'rotate_normalized_labels'] if 'rotate_normalized_labels' in prior_hyperparameters else True
-
-    use_style = False
-
-    if 'differentiable' in config and config['differentiable']:
-        get_batch_base = make_get_batch(model_proto, **extra_kwargs)
-        extra_kwargs = {'get_batch': get_batch_base, 'differentiable_hyperparameters': config['differentiable_hyperparameters']}
-        model_proto = priors.differentiable_prior
-        use_style = True
-    print(f"Using style prior: {use_style}")
+    
 
     if (('nan_prob_no_reason' in config and config['nan_prob_no_reason'] > 0.0) or
         ('nan_prob_a_reason' in config and config['nan_prob_a_reason'] > 0.0) or
@@ -306,7 +178,6 @@ def get_model(config, device, should_train=True, verbose=False, state_dict=None,
         raise ValueError(f"Invalid number of classes: {config['max_num_classes']}")
 
     aggregate_k_gradients = 1 if 'aggregate_k_gradients' not in config else config['aggregate_k_gradients']
-    check_is_compatible = False if 'multiclass_loss_type' not in config else (config['multiclass_loss_type'] == 'compatible')
     config['multiclass_type'] = config['multiclass_type'] if 'multiclass_type' in config else 'rank'
     config['mix_activations'] = config['mix_activations'] if 'mix_activations' in config else False
 
@@ -315,7 +186,6 @@ def get_model(config, device, should_train=True, verbose=False, state_dict=None,
 
     model_maker = config.get('model_maker', False)
     epochs = 0 if not should_train else config['epochs']
-    #print('MODEL BUILDER', model_proto, extra_kwargs['get_batch'])
     if 'y_encoder' not in config:
         config['y_encoder'] = 'one_hot'
 
@@ -326,18 +196,13 @@ def get_model(config, device, should_train=True, verbose=False, state_dict=None,
     else:
         raise ValueError(f"Unknown y_encoder: {config['y_encoder']}")
 
-    extra_prior_kwargs_dict={
-                'num_features': config['num_features']
-                , 'hyperparameters': prior_hyperparameters
-                #, 'dynamic_batch_size': 1 if ('num_global_att_tokens' in config and config['num_global_att_tokens']) else 2
-                , 'batch_size_per_gp_sample': config.get('batch_size_per_gp_sample', None)
-                , **extra_kwargs
-            }
 
-    dataloader_config = dict(steps_per_epoch=config['num_steps'], batch_size=config['batch_size'], bptt=config['bptt'], bptt_extra_samples=config['bptt_extra_samples'], device=device,
-                             single_eval_pos_gen=get_uniform_single_eval_pos_sampler(config.get('max_eval_pos', config['bptt']), min_len=config.get('min_eval_pos', 0)),
-                             **extra_prior_kwargs_dict)
-    dl = get_dataloader(model_proto.DataLoader, **dataloader_config)
+    dataloader_config = dict(steps_per_epoch=config['num_steps'], batch_size=config['batch_size'], bptt=config['bptt'],
+                             bptt_extra_samples=config['bptt_extra_samples'], device=device,
+                             single_eval_pos_gen=get_uniform_single_eval_pos_sampler(config.get('max_eval_pos', config['bptt']),
+                                                                                     min_len=config.get('min_eval_pos', 0)))
+    dl = get_dataloader(config['prior_type'], config['flexible'], config['differentiable'], config,
+                        **dataloader_config)
 
     model = assemble_model(encoder_generator=encoder, y_encoder=y_encoder, num_features=config['num_features'], emsize=config['emsize'], nhead=config['nhead'],
                            nhid=config['emsize'] * config['nhid_factor'], nlayers=config['nlayers'], dropout=config['dropout'],
