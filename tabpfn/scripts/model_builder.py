2bef9115ab8bc140e886f687919c2dfd39d64c4b
Andreas C Mueller <amueller@microsoft.com>
2023-05-30 21:35:25 +0000

    serialize using cloudpickle
    Former-commit-id: 2b175c209e252944b32f4a77837dd29daec063b0

--- tabpfn/scripts/model_builder.py
+++ tabpfn/scripts/model_builder.py
@@ -15,24 +15,8 @@ import math
 def save_model(model, path, filename, config_sample):
     config_sample = {**config_sample}
 
-    def make_serializable(config_sample):
-        if isinstance(config_sample, dict):
-            config_sample = {k: make_serializable(config_sample[k]) for k in config_sample}
-        if isinstance(config_sample, list):
-            config_sample = [make_serializable(v) for v in config_sample]
-        if callable(config_sample):
-            config_sample = str(config_sample)
-        return config_sample
-
-    #if 'num_features_used' in config_sample:
-    #    del config_sample['num_features_used']
-
-    #config_sample['num_classes_as_str'] = str(config_sample['num_classes'])
-    #del config_sample['num_classes']
-
-    config_sample = make_serializable(config_sample)
-
-    torch.save((model.state_dict(), None, config_sample), os.path.join(path, filename))
+    import cloudpickle
+    torch.save((model.state_dict(), None, config_sample), os.path.join(path, filename), pickle_module=cloudpickle)
 
 
 
