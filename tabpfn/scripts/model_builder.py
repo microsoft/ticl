8ae1bc2159a2bdf0350df049dff1b9c13b1d70a1
Andreas Mueller <amueller@microsoft.com>
2023-05-30 17:44:27 +0000

    add predicted_hidden_layer_size to assemble_model
    Former-commit-id: dccff97fa4a9fad03c1caecf7163f5bf0b2cb310

--- tabpfn/scripts/model_builder.py
+++ tabpfn/scripts/model_builder.py
@@ -211,6 +211,7 @@ def get_model(config, device, should_train=True, verbose=False, state_dict=None,
     model = assemble_model(encoder_generator=encoder, y_encoder=y_encoder, num_features=config['num_features'], emsize=config['emsize'], nhead=config['nhead'],
                            nhid=config['emsize'] * config['nhid_factor'], nlayers=config['nlayers'], dropout=config['dropout'],
                            input_normalization=config.get('input_normalization', False),  model_maker=model_maker, criterion=loss,
+                           predicted_hidden_layer_size=config.get('predicted_hidden_layer_size', None),
                            load_weights_from_this_state_dict=state_dict, load_model_strict=load_model_strict)
     model = train(dl,
                   model,
