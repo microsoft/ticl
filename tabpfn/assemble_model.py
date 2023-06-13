78b1315065d11c5765ac11764589b6c3b1ceb153
amueller <amueller@microsoft.com>
2023-06-13 17:33:13 +0000

    add missing file
    Former-commit-id: a6a1b4fc1e80f5db82c0b636cafc8e11f4569791

--- tabpfn/assemble_model.py
+++ tabpfn/assemble_model.py
@@ -7,7 +7,7 @@ def assemble_model(encoder_generator, num_features, emsize, nhead, nhid, nlayers
                    model_maker, max_num_classes, efficient_eval_masking=False,
                    output_attention=False, special_token=False, predicted_hidden_layer_size=None, decoder_embed_dim=None,
                    decoder_hidden_size=None, decoder_two_hidden_layers=False, no_double_embedding=False,
-                   load_weights_from_this_state_dict=None, load_model_strict=True, verbose=False, **model_extra_args):
+                   load_weights_from_this_state_dict=None, load_model_strict=True, verbose=False, pre_norm=False, **model_extra_args):
     encoder = encoder_generator(num_features, emsize)
     decoder_hidden_size = decoder_hidden_size or nhid
 
@@ -21,7 +21,7 @@ def assemble_model(encoder_generator, num_features, emsize, nhead, nhid, nlayers
                                 efficient_eval_masking=efficient_eval_masking, output_attention=output_attention, special_token=special_token,
                                 predicted_hidden_layer_size=predicted_hidden_layer_size, decoder_embed_dim=decoder_embed_dim,
                                 decoder_hidden_size=decoder_hidden_size, decoder_two_hidden_layers=decoder_two_hidden_layers,
-                                no_double_embedding=no_double_embedding,
+                                no_double_embedding=no_double_embedding, pre_norm=pre_norm,
                                 **model_extra_args
                                 )
     elif model_maker == 'perceiver':
@@ -35,12 +35,12 @@ def assemble_model(encoder_generator, num_features, emsize, nhead, nhid, nlayers
     elif model_maker:
         model = TransformerModelMaker(encoder, n_out, emsize, nhead, nhid, nlayers, dropout,
                                 y_encoder=y_encoder, input_normalization=input_normalization,
-                                efficient_eval_masking=efficient_eval_masking, **model_extra_args
+                                efficient_eval_masking=efficient_eval_masking, pre_norm=pre_norm, **model_extra_args
                                 )
     else:
         model = TransformerModel(encoder, n_out, emsize, nhead, nhid, nlayers, dropout,
                                 y_encoder=y_encoder, input_normalization=input_normalization,
-                                efficient_eval_masking=efficient_eval_masking, **model_extra_args
+                                efficient_eval_masking=efficient_eval_masking, pre_norm=pre_norm, **model_extra_args
                                 )
     if load_weights_from_this_state_dict is not None:
         model.load_state_dict(load_weights_from_this_state_dict, strict=load_model_strict)
