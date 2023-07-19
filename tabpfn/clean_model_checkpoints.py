eda35fc27af3fda127f3d9a68417bf8314334e14
Andreas Mueller <amueller@microsoft.com>
2023-07-19 17:36:41 +0000

    add safer script for deleting model checkpoints
    Former-commit-id: db1d8be1636c1e00a239f4bbc2f3b7bf1b8d2caa

---
+++ tabpfn/clean_model_checkpoints.py
@@ -0,0 +1,35 @@
+import glob
+import re
+import os
+from collections import defaultdict
+import torch
+
+file_dict = defaultdict(list)
+for name in glob.glob("models_diff/*.cpkt"):
+    shortname = name.split("/")[1][len("prior_diff_real_checkpoint_"):-5]
+    canonical_name = shortname.split("epoch")[0][:-5]
+    file_dict[canonical_name].append(name)
+
+
+canonical_file = dict()
+for canonical_name, files in file_dict.items():
+    last_epoch = -1
+    for f in files:
+        if "on_exit" in f:
+            canonical_file[canonical_name] = f
+            break
+        epoch = int(re.findall("(\d+)", f.split("epoch")[1])[0])
+        if epoch > last_epoch:
+            last_epoch = epoch
+            canonical_file[canonical_name] = f
+            
+for canonical_name, files in file_dict.items():
+    try:
+        torch.load(canonical_file[canonical_name])
+    except Exception as e:
+        print(f"Error loading {canonical_file[canonical_name]}")
+        print(e)
+        continue
+    for file in files:
+        if file != canonical_file[canonical_name]:
+            os.remove(file)
