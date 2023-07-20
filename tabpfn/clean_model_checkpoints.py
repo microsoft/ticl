68798b02f75580a277112b9454f468550774bd16
amueller <amueller@microsoft.com>
2023-07-20 14:23:39 +0000

    don't rely on final save cause it doesn't have the optimizer state
    Former-commit-id: 634c6302af82816b78896ab2fde101490358d4e3

--- tabpfn/clean_model_checkpoints.py
+++ tabpfn/clean_model_checkpoints.py
@@ -7,7 +7,7 @@ import torch
 file_dict = defaultdict(list)
 for name in glob.glob("models_diff/*.cpkt"):
     shortname = name.split("/")[1]
-    canonical_name = shortname.split("epoch")[0]
+    canonical_name = shortname.split("epoch")[0].strip("_")
     file_dict[canonical_name].append(name)
 
 
@@ -16,8 +16,8 @@ for canonical_name, files in file_dict.items():
     last_epoch = -1
     for f in files:
         if "on_exit" in f:
-            canonical_file[canonical_name] = f
-            break
+            # we don't count that one as the final one because it doesn't have the optimizer state
+            continue
         epoch = int(re.findall("(\d+)", f.split("epoch")[1])[0])
         if epoch > last_epoch:
             last_epoch = epoch
