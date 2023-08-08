c297005965f76586ca73c64d32113054647975a1
amueller <amueller@microsoft.com>
2023-08-08 17:32:56 +0000

    add some more tests
    Former-commit-id: 292f48c518b364c415fc52c70a3616fb6730570a

--- tabpfn/tests/mothernet_prediction/test_transformer_make_model.py
+++ tabpfn/tests/mothernet_prediction/test_transformer_make_model.py
@@ -2,17 +2,22 @@ from tabpfn.transformer_make_model import ShiftClassifier, EnsembleMeta, Forward
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
+import pytest
+
 MOTHERNET_PATH = "models_diff/prior_diff_real_checkpointcontinue_hidden_128_embed_dim_1024_decoder_nhid_2048_nlayer12_lr0003_n_0_epoch_on_exit.cpkt"
 TABPFN_PATH = ""
 
-
-def test_basic_test_iris():
+@pytest.mark.parametrize("ensemble", [ShiftClassifier, EnsembleMeta, None])
+@pytest.mark.parametrize("class_offset", [0, 4])
+def test_basic_test_iris(ensemble, class_offset):
     iris = load_iris()
     X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, random_state=42)
     mothernet = ForwardMLPModel(path=MOTHERNET_PATH, device='cpu')
-    mothernet.fit(X_train, y_train)
+    if ensemble is not None:
+        mothernet = ensemble(mothernet)
+    mothernet.fit(X_train, y_train + class_offset)
     pred = mothernet.predict(X_test)
     assert pred.shape == (38,)
     pred_prob = mothernet.predict_proba(X_test)
     assert pred_prob.shape == (38, 3)
-    assert mothernet.score(X_test, y_test) > 0.9
+    assert mothernet.score(X_test, y_test + class_offset) > 0.9
