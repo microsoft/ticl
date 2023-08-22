df4ff5d0bd2d7bbb87b5506ea128ed722bfe5df1
amueller <amueller@microsoft.com>
2023-08-22 22:54:11 +0000

    update training test for prenorm default, add defaults for d and H
    Former-commit-id: 4434bf2d719c8c109ee0d7221c1cc9a17d3019a4

--- tabpfn/tests/test_training.py
+++ tabpfn/tests/test_training.py
@@ -10,42 +10,42 @@ import lightning as L
 
 # really tiny model for smoke tests
 # one step per epoch, no adapting batchsize, CPU, Mothernet
-TESTING_DEFAULTS = ['-C', '-E', '10', '-n', '1', '-A', '-e', '128', '-N', '4', '-P', '64', '--experiment', 'testing_experiment']
+TESTING_DEFAULTS = ['-C', '-E', '10', '-n', '1', '-A', '-e', '128', '-N', '4', '-P', '64', '-H', '128', '-d', '128', '--experiment', 'testing_experiment']
 
 
 def test_train_defaults():
     L.seed_everything(42)
     with tempfile.TemporaryDirectory() as tmpdir:
         loss, model, _ = main(TESTING_DEFAULTS + ['-B', tmpdir])
-    assert loss == 2.350903034210205
+    assert loss == 2.4132816791534424
     assert isinstance(model, TransformerModelMakeMLP)
 
 def test_train_double_embedding():
     L.seed_everything(42)
     with tempfile.TemporaryDirectory() as tmpdir:
         loss, model, _ = main(TESTING_DEFAULTS + ['-B', tmpdir, '-D'])
-    assert loss == 2.313856601715088
+    assert loss == 2.2314932346343994
     assert isinstance(model, TransformerModelMakeMLP)
 
 def test_train_special_token():
     L.seed_everything(42)
     with tempfile.TemporaryDirectory() as tmpdir:
         loss, model, _ = main(TESTING_DEFAULTS + ['-B', tmpdir, '-S'])
-    assert loss == 3.088744878768921
+    assert loss == 2.3119993209838867
     assert isinstance(model, TransformerModelMakeMLP)
 
 def test_train_two_hidden_layers():
     L.seed_everything(42)
     with tempfile.TemporaryDirectory() as tmpdir:
         loss, model, _ = main(TESTING_DEFAULTS + ['-B', tmpdir, '-L' '2'])
-    assert loss == 2.2727622985839844
+    assert loss == 2.3295023441314697
     assert isinstance(model, TransformerModelMakeMLP)
 
 def test_train_low_rank():
     L.seed_everything(42)
     with tempfile.TemporaryDirectory() as tmpdir:
         loss, model, _ = main(TESTING_DEFAULTS + ['-B', tmpdir, '-W' '16'])
-    assert loss == 2.243964910507202
+    assert loss == 2.3065733909606934
     assert isinstance(model, TransformerModelMakeMLP)
 
 def test_train_tabpfn():
