9c88ac3e32a5568a81ee9ae244f1513af053fc93
amueller <amueller@microsoft.com>
2023-11-02 03:34:08 +0000

    add plotting from mlflow experiment
    Former-commit-id: 17fa2f41fb73eb312801dfe2384bd8f144d822ca

--- tabpfn/fit_learning_curve.py
+++ tabpfn/fit_learning_curve.py
@@ -68,7 +68,7 @@ def plot_exponential_regression(loss_df, x='epoch', y='loss', hue='run', extrapo
     fig.update_layout(xaxis_title=x, yaxis_title=y, height=800)
     return fig
 
-def plot_exponential_smoothing(loss_df, x='time_days', y='loss', hue='run', extra_smoothing=1):
+def plot_exponential_smoothing(loss_df, x='time_days', y='loss', hue='run', extra_smoothing=1, xlog=True, ylog=True):
     import plotly.graph_objects as go
     fig = go.Figure()
     for run in loss_df[hue].unique():
@@ -79,4 +79,44 @@ def plot_exponential_smoothing(loss_df, x='time_days', y='loss', hue='run', extr
         else:
             fig.add_trace(go.Scatter(x=smoothed[x], y=smoothed[y], mode='lines', name=run, hoverinfo="name", hoverlabel_namelength=-1))
     fig.update_layout(height=800)
+    if xlog:
+        fig.update_xaxes(type="log")
+    if ylog:
+        fig.update_yaxes(type="log")
+    fig.update_xaxes(minor=dict(ticks="inside", ticklen=6, showgrid=True))
+    return fig
+
+
+import pandas as pd
+from mlflow import MlflowClient
+from mlflow.entities import ViewType
+from mlflow.exceptions import MlflowException
+
+def plot_experiment(experiment_name, x="epoch", verbose=False):
+    experiment_id=MlflowClient().get_experiment_by_name(experiment_name).experiment_id
+    runs_running = MlflowClient().search_runs(
+                experiment_ids=experiment_id,
+                run_view_type=ViewType.ACTIVE_ONLY,
+                order_by=["metrics.accuracy DESC"])
+
+    losses_all = []
+    already_seen = set()
+    for run in runs_running:
+        if run.info.run_id in already_seen:
+            continue
+        already_seen.add(run.info.run_id)
+        try:
+            losses = MlflowClient().get_metric_history(run.info.run_id, key="loss")
+            losses_df = pd.DataFrame.from_dict([dict(l) for l in losses])
+            losses_df['run'] = run.info.run_name
+            losses_df['timestamp'] -= losses_df.timestamp.min()
+            losses_df['time_days'] = losses_df['timestamp'] / 1000 / (60 * 60 * 24)
+            losses_df['status'] = run.info.status
+            losses_all.append(losses_df)
+        except MlflowException as e:
+            if verbose:
+                print(e)
+    losses_all_df = pd.concat(losses_all, ignore_index=True).rename(columns={'value':'loss', 'step': 'epoch'})
+    fig = plot_exponential_smoothing(losses_all_df, x=x, y='loss')
+    fig.update_layout(showlegend=False)
     return fig
