807d533d04fa3d496492edb0f88d71026b4f033b
amueller <amueller@microsoft.com>
2023-11-07 21:03:43 +0000

    too easy
    Former-commit-id: bdd270eed46c9e9a1707a508adf1856285de2f96

--- tabpfn/tuning_scripts/train_test_timing.py
+++ tabpfn/tuning_scripts/train_test_timing.py
@@ -3,11 +3,11 @@ import time
 import os
 import numpy as np
 from pathlib import Path
-from time import time
+import time
 
 from syne_tune import Reporter
 from argparse import ArgumentParser
-
+import pickle
 
 if __name__ == '__main__':
 
@@ -22,20 +22,21 @@ if __name__ == '__main__':
     report = Reporter()
     start = 1
     tick = time.time()
+    extra_time = 0
     checkpoint_dir = args.st_checkpoint_dir
     if checkpoint_dir is not None:
         os.makedirs(checkpoint_dir, exist_ok=True)
         checkpoint_path = Path(checkpoint_dir) / "checkpoint.iter"
         if checkpoint_path.exists():
-            with open(checkpoint_path, "r") as f:
-                state = int(f.read())
+            with open(checkpoint_path, "rb") as f:
+                state = pickle.load(f)
                 start = state['epoch']
                 extra_time = state['time']
 
     for i in range(start, args.epochs):
         time.sleep(0.1 * (args.parameter + 1))
         current_time = time.time() - tick + extra_time
-        report(epoch=i, loss=np.exp(-1/1000 *  (np.sqrt(args.parameter + 1) * i), time=current_time))
+        report(epoch=i, loss=np.exp(-1/1000 *  (np.sqrt(args.parameter + 1) * i)), time=current_time)
         if checkpoint_dir is not None:
-            with open(checkpoint_path, "w") as f:
-                f.write({'epoch': i, 'time': current_time})
+            with open(checkpoint_path, "wb") as f:
+                pickle.dump({'epoch': i, 'time': current_time}, f)
