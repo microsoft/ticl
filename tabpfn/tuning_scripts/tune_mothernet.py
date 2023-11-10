d10c7a52f22c38c86a70172e4f317369090952f1
amueller <amueller@microsoft.com>
2023-11-10 02:25:28 +0000

    start timed mothernet experiments
    Former-commit-id: 4057b90dd98e986e3d4a1db937c6a6b1cb535979

--- tabpfn/tuning_scripts/tune_mothernet.py
+++ tabpfn/tuning_scripts/tune_mothernet.py
@@ -2,12 +2,12 @@ import logging
 
 from syne_tune import Tuner, StoppingCriterion
 from syne_tune.backend import LocalBackend
-from syne_tune.config_space import randint, loguniform, uniform, lograndint, choice, logfinrange
+from syne_tune.config_space import randint, loguniform, uniform, choice, logfinrange
 from syne_tune.optimizer.baselines import ASHA, MOBSTER, HyperTune
 root = logging.getLogger()
 root.setLevel(logging.INFO)
 
-tuner_name = "mothernet-delete-checkpoints"
+tuner_name = "mothernet-big-searchspace-timed"
 
 
 # hyperparameter search space to consider
@@ -19,6 +19,7 @@ config_space = {
     'batch-size': logfinrange(lower=2, upper=32, size=5, cast_int=True),
     'adaptive-batch-size': choice([True, False]),
     'decoder-two-hidden-layers': choice([True, False]),
+    'special-token': choice([True, False]),
     'weight-decay': loguniform(1e-9, 1e-1),
     'num-predicted-hidden-layers': randint(1, 6),
     'low_rank_weights': choice([True, False]),
@@ -27,11 +28,13 @@ config_space = {
     'learning-rate-schedule': choice(['exponential']),
     'adam-beta1': uniform(0.8, 0.999),
     'lr-decay': uniform(0.90, 0.9999),
-    'reduce-lr-on-spike': choice([True]),
+    'reduce-lr-on-spike': choice([True, False]),
     'save-every': 1,
     'spike-tolerance': randint(1, 10),
     'experiment': f'synetune-{tuner_name}',
     'warmup-epochs': randint(0, 30),
+    'decoder-em-size': logfinrange(lower=128, upper=4096, size=6, cast_int=True),
+    'decoder-hidden-size': logfinrange(lower=128, upper=4096, size=6, cast_int=True),
 }
 early_checkpoint_removal_kwargs = {"max_num_checkpoints": 80}
 
@@ -41,12 +44,12 @@ tuner = Tuner(
         scheduler=MOBSTER(
         config_space,
         metric='loss',
-        resource_attr='epoch',
-        max_resource_attr="stop_after_epochs",
+        resource_attr='wallclock_time',
+        max_resource_attr="wallclock_time",
         search_options={'debug_log': False},
         mode='min',
         type="promotion",
-        grace_period=5,
+        grace_period=10,
         early_checkpoint_removal_kwargs=early_checkpoint_removal_kwargs,
 
     ),
@@ -55,7 +58,7 @@ tuner = Tuner(
     print_update_interval=120,
     #stop_criterion=StoppingCriterion(max_wallclock_time=60 *60),
     stop_criterion=StoppingCriterion(max_num_trials_started=5000),
-    n_workers=2,  # how many trials are evaluated in parallel
+    n_workers=4,  # how many trials are evaluated in parallel
     tuner_name=tuner_name,
     trial_backend_path=f"/synetune_checkpoints/{tuner_name}/"
 )
