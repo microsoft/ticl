830b938041eb1f24558765ef796a431d509617f3
amueller <amueller@microsoft.com>
2023-11-10 02:53:42 +0000

    use tuner name from command line, add all learning rate schedules back
    Former-commit-id: 53838e1fb0824f6fc1add107dcedd5ff4180986e

--- tabpfn/tuning_scripts/tune_mothernet.py
+++ tabpfn/tuning_scripts/tune_mothernet.py
@@ -4,11 +4,10 @@ from syne_tune import Tuner, StoppingCriterion
 from syne_tune.backend import LocalBackend
 from syne_tune.config_space import randint, loguniform, uniform, choice, logfinrange
 from syne_tune.optimizer.baselines import ASHA, MOBSTER, HyperTune
+import sys
+
 root = logging.getLogger()
 root.setLevel(logging.INFO)
-
-tuner_name = "mothernet-big-searchspace-timed3"
-
 
 # hyperparameter search space to consider
 config_space = {
@@ -25,13 +24,12 @@ config_space = {
     'low-rank-weights': choice([True, False]),
     'weight-embedding-rank': logfinrange(lower=16, upper=512, size=6, cast_int=True),
     'predicted-hidden-layer-size': logfinrange(lower=32, upper=1024, size=6, cast_int=True),
-    'learning-rate-schedule': choice(['exponential']),
+    'learning-rate-schedule': choice(['exponential', 'constant', 'cosine']),
     'adam-beta1': uniform(0.8, 0.999),
     'lr-decay': uniform(0.90, 0.9999),
     'reduce-lr-on-spike': choice([True, False]),
     'save-every': 1,
     'spike-tolerance': randint(1, 10),
-    'experiment': f'synetune-{tuner_name}',
     'warmup-epochs': randint(0, 30),
     'decoder-em-size': logfinrange(lower=128, upper=4096, size=6, cast_int=True),
     'decoder-hidden-size': logfinrange(lower=128, upper=4096, size=6, cast_int=True),
@@ -39,27 +37,33 @@ config_space = {
 early_checkpoint_removal_kwargs = {"max_num_checkpoints": 80}
 
 
-tuner = Tuner(
-    trial_backend=LocalBackend(entry_point='../fit_model.py'),
-        scheduler=MOBSTER(
-        config_space,
-        metric='loss',
-        resource_attr='wallclock_time',
-        max_resource_attr="wallclock_time",
-        search_options={'debug_log': False},
-        mode='min',
-        type="promotion",
-        grace_period=10,
-        early_checkpoint_removal_kwargs=early_checkpoint_removal_kwargs,
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python tune_mothernet.py <tuner_name>")
+        exit(1)
+    tuner_name = sys.argv[1]
+    config_space['experiment'] = f'synetune-{tuner_name}'
+    tuner = Tuner(
+        trial_backend=LocalBackend(entry_point='../fit_model.py'),
+            scheduler=MOBSTER(
+            config_space,
+            metric='loss',
+            resource_attr='wallclock_time',
+            max_resource_attr="wallclock_time",
+            search_options={'debug_log': False},
+            mode='min',
+            type="promotion",
+            grace_period=10,
+            early_checkpoint_removal_kwargs=early_checkpoint_removal_kwargs,
 
-    ),
-    max_failures=1000,
-    results_update_interval=60,
-    print_update_interval=120,
-    #stop_criterion=StoppingCriterion(max_wallclock_time=60 *60),
-    stop_criterion=StoppingCriterion(max_num_trials_started=5000),
-    n_workers=4,  # how many trials are evaluated in parallel
-    tuner_name=tuner_name,
-    trial_backend_path=f"/synetune_checkpoints/{tuner_name}/"
-)
-tuner.run()
+        ),
+        max_failures=1000,
+        results_update_interval=60,
+        print_update_interval=120,
+        #stop_criterion=StoppingCriterion(max_wallclock_time=60 *60),
+        stop_criterion=StoppingCriterion(max_num_trials_started=5000),
+        n_workers=4,  # how many trials are evaluated in parallel
+        tuner_name=tuner_name,
+        trial_backend_path=f"/synetune_checkpoints/{tuner_name}/"
+    )
+    tuner.run()
