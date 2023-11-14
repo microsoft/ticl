5101754865c53da136f6c53feca1de611d130811
Andreas Mueller <amueller@microsoft.com>
2023-11-13 17:39:55 -0800

    actually check in required files
    Former-commit-id: 3a6d67a34d5b048a673b70732c5a879e6bf8bac5

--- tabpfn/utils.py
+++ tabpfn/utils.py
@@ -6,6 +6,7 @@ import datetime
 import itertools
 import glob
 import re
+import time
 
 import torch
 from torch import nn
@@ -16,6 +17,9 @@ from scipy.signal import convolve, windows
 
 from torch.optim.optimizer import Optimizer
 
+import socket
+import shutil
+import mlflow
 
 
 def get_openai_lr(transformer_model):
@@ -432,3 +436,216 @@ class ReduceLROnSpike:
        """ Return last computed learning rate by current scheduler.
        """
        return self._last_lr
+    
+
+def make_base_parser(description):
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-g', '--gpu-id', type=int, help='GPU id')
+    parser.add_argument('-e', '--em-size', type=int, help='embedding size', default=512)
+    parser.add_argument('-n', '--num-steps', type=int, help='number of steps per epoch')
+    parser.add_argument('-E', '--epochs', type=int, help='number of epochs', default=4000)
+    parser.add_argument('-l', '--learning-rate', type=float, help='maximum learning rate', default=0.00003)
+    parser.add_argument('-N', '--num-layers', type=int, help='number of transformer layers', default=12)
+    parser.add_argument('-k', '--agg-gradients', type=int, help='number steps to aggregate gradient over', default=1)
+    parser.add_argument('-b', '--batch-size', type=int, help='physical batch size', default=8)
+    parser.add_argument('-A', '--adaptive-batch-size', help='Wether to progressively increase effective batch size.', default=True, type=str2bool)
+    parser.add_argument('-w', '--weight-decay', type=float, help='Weight decay for AdamW.', default=0)
+    parser.add_argument('-f', '--load-file', help='Warm start from this file')
+    parser.add_argument('-c', '--continue-run', help='Whether to read the old config when warm starting', action='store_true')
+    parser.add_argument('-s', '--load-strict', help='Whether to load the architecture strictly when warm starting', action='store_true')
+    parser.add_argument('--restart-scheduler', help='Whether to restart the scheduler when warm starting', action='store_true')
+    parser.add_argument('-C', '--use-cpu', help='whether to use cpu', action='store_true')
+    parser.add_argument('-R', '--create-new-run', help="Create as new MLFLow run, even if continuing", action='store_true')
+    parser.add_argument('-Q', '--learning-rate-schedule', help="Learning rate schedule. Cosine, constant or exponential", default='cosine')
+    parser.add_argument('-U', '--warmup-epochs', type=int, help="Number of epochs to warm up learning rate (linear climb)", default=20)
+    parser.add_argument('-t', '--train-mixed-precision', help='whether to train with mixed precision', default=True, type=bool)
+    parser.add_argument('--adam-beta1', default=0.9, type=float)
+    parser.add_argument('--experiment', help="Name of mlflow experiment", default='Default')
+    parser.add_argument('--lr-decay', help="learning rate decay when using exponential schedule", default=0.99, type=float)
+    parser.add_argument('-B', '--base-path', default='.')
+    parser.add_argument('--pre-norm', action='store_true')
+    parser.add_argument('--reduce-lr-on-spike', help="Whether to half learning rate when observing a loss spike", default=True, type=str2bool)
+    parser.add_argument('--save-every', default=10, type=int)
+    parser.add_argument('--spike-tolerance', help="how many times the std makes it a spike", default=4, type=int)
+    parser.add_argument('--st_checkpoint_dir', help="checkpoint dir for synetune", type=str, default=None)
+    parser.add_argument('--stop-after-epochs', help="for pausing rungs with synetune", type=int, default=None)
+
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise ValueError("Boolean value expected.")
+
+
+def init_device(gpu_id, use_cpu):
+        # Single GPU training, get GPU ID from command line
+    if 'LOCAL_RANK' in os.environ:
+        # launched with torch.distributed.launch
+        rank = int(os.environ["LOCAL_RANK"])
+        print('torch.distributed.launch and my rank is', rank)
+        num_gpus = int(os.environ["WORLD_SIZE"])
+        raise ValueError("Gave up on multi-gpu for now")
+    else:
+        rank = 0
+        num_gpus = 1
+
+    device = "cuda"
+    if gpu_id is not None:
+        if use_cpu:
+            raise ValueError("Can't use cpu and gpu at the same time")
+        device = f'cuda:{gpu_id}'
+    elif use_cpu:
+        device = 'cpu'
+
+    return device, rank, num_gpus
+
+
+def get_model_string(config, args, parser):
+    if args.continue_run:
+        if checkpoint_dir is None:
+            model_string = warm_start_weights.split("/")[-1].split("_epoch_")[0]
+            if args.create_new_run:
+                model_string = model_string + '_continue_'+datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+        else:
+            with open(f"{checkpoint_dir}/model_string.txt", 'r') as f:
+                model_string = f.read()
+    else:
+        mm = config['model_maker']
+        model_maker_string = mm if mm in ["perceiver", "additive"] else ('mn' if mm == "mlp" else "tabpfn")
+
+        default_args_dict = vars(parser.parse_args([]))
+        args_dict = vars(args)
+        config_string = ""
+        for arg in parser._actions:
+            if arg.option_strings:
+                k = arg.dest
+                if k in ['st_checkpoint_dir', 'save_every', 'run_id', 'load_file', 'use_cpu', 'continue_run', 'restart_scheduler', 'load_strict', 'gpu_id', 'help', 'base_path', 'create_new_run', 'experiment', 'model_maker'] or k not in args_dict:
+                    continue
+                v = args_dict[k]
+                short_name = arg.option_strings[0].replace('-', '')
+                if v != default_args_dict[k]:
+                    if isinstance(v, float):
+                        config_string += f"_{short_name}{v:.4g}"
+                    else:
+                        config_string += f"_{short_name}{v}"
+        gpu_string = f"_{config['num_gpus']}_gpu{'s' if config['num_gpus'] > 1 else ''}" if config['device'] != 'cpu' else '_cpu'
+        model_string = f"{model_maker_string}{config_string}{gpu_string}{'_continue' if args.continue_run else '_warm' if args.load_file else ''}"
+        model_string = model_string + '_'+datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+        if checkpoint_dir is not None:
+            with open(f"{checkpoint_dir}/model_string.txt", 'w') as f:
+                f.write(model_string)
+
+
+def save_callback(model, optimizer, scheduler, epoch):
+    if not hasattr(model, 'last_saved_epoch'):
+        model.last_saved_epoch = 0
+    log_file = f'{base_path}/log/{model_string}.log'
+    if epoch == "start":
+        print(f"Starting training of model {model_string}")
+        return
+    try:
+        os.makedirs(f"{base_path}/log", exist_ok=True)
+        with open(log_file, 'a') as f:
+            f.write(f'Epoch {epoch} loss {model.losses[-1]} learning_rate {model.learning_rates[-1]}\n')
+    except Exception as e:
+        print(f'Failed to write to log file {log_file}: {e}')
+
+    if epoch != "on_exit":
+        mlflow.log_metric(key="wallclock_time", value=model.wallclock_times[-1], step=epoch)
+        mlflow.log_metric(key="loss", value=model.losses[-1], step=epoch)
+        mlflow.log_metric(key="learning_rate", value=model.learning_rates[-1], step=epoch)
+        wallclock_ticker = max(1, int(model.wallclock_times[-1]//(60 * 5)))
+        mlflow.log_metric(key="wallclock_ticker", value=wallclock_ticker, step=epoch)
+        report(epoch=epoch, loss=model.losses[-1], wallclock_time=wallclock_ticker)  # every 5 minutes
+
+    try:
+        if (epoch == "on_exit") or epoch % save_every == 0:
+            if checkpoint_dir is not None:
+                if epoch == "on_exit":
+                    return
+                file_name = f'{base_path}/checkpoint.mothernet'
+            else:
+                file_name = f'{base_path}/models_diff/{model_string}_epoch_{epoch}.cpkt'
+            os.makedirs(f"{base_path}/models_diff", exist_ok=True)
+            disk_usage = shutil.disk_usage(f"{base_path}/models_diff")
+            if disk_usage.free < 1024 * 1024 * 1024 * 2:
+                print("Not saving model, not enough disk space")
+                print("DISK FULLLLLLL")
+                return
+            with open(log_file, 'a') as f:
+                f.write(f'Saving model to {file_name}\n')
+            print(f'Saving model to {file_name}')
+            config_sample['epoch_in_training'] = epoch
+            config_sample['learning_rates'] = model.learning_rates
+            config_sample['losses'] = model.losses
+            config_sample['wallclock_times'] = model.wallclock_times
+
+            save_model(model, optimizer, scheduler, base_path, file_name, config_sample)
+            # remove checkpoints that are worse than current
+            if epoch != "on_exit" and epoch - save_every > 0:
+                this_loss = model.losses[-1]
+                for i in range(epoch // save_every):
+                    loss = model.losses[i * save_every - 1]  # -1 because we start at epoch 1
+                    old_file_name = f'{base_path}/models_diff/{model_string}_epoch_{i * save_every}.cpkt'
+                    if os.path.exists(old_file_name):
+                        if loss > this_loss:
+                                try:
+                                    print(f"Removing old model file {old_file_name}")
+                                    os.remove(old_file_name)
+                                except Exception as e:
+                                    print(f"Failed to remove old model file {old_file_name}: {e}")
+                        else:
+                            print(f"Not removing old model file {old_file_name} because loss is too high ({loss} < {this_loss})")
+    
+    except Exception as e:
+        print("WRITING TO MODEL FILE FAILED")
+        print(e)
+
+def load_model_state(load_path, config):
+    model_state, old_optimizer_state, old_scheduler, old_config = torch.load(
+        load_path, map_location='cpu')
+    module_prefix = 'module.'
+    model_state = {k.replace(module_prefix, ''): v for k, v in model_state.items()}
+    if config['continue_run']:
+        config_sample = old_config
+        config_sample['device'] = config['device']
+        config_sample['warm_start_from'] = load_path
+        optimizer_state = old_optimizer_state
+        config_sample['stop_after_epochs'] = config['stop_after_epochs']
+        if not config['restart_scheduler']:
+            scheduler = old_scheduler
+    else:
+        print("WARNING warm starting with new settings")
+        compare_dicts(config_sample, old_config, all=True)
+    
+    return model_state, optimizer_state, scheduler, config_sample
+
+def init_mlflow(experiment_name, model_string, continue_run):
+    if socket.gethostname() == "amueller-tabpfn-4gpu":
+        mlflow.set_tracking_uri("http://localhost:5000")
+    else:
+        mlflow.set_tracking_uri("http://20.114.249.177:5000")
+
+    tries = 0
+    while tries < 5:
+        try:
+            mlflow.set_experiment(experiment_name)
+            break
+        except:
+            tries += 1
+            print(f"Failed to set experiment, retrying {tries}/5")
+            time.sleep(5)
+
+    if continue_run:
+        # find run id via mlflow
+        run_ids = mlflow.search_runs(filter_string=f"attribute.run_name='{model_string}'")['run_id']
+        if len(run_ids) > 1:
+            raise ValueError(f"Found more than one run with name {model_string}")
+        run_id = run_ids.iloc[0]
+        run_args = {'run_id': run_id}
+
+    else:
+        run_args = {'run_name': model_string}
