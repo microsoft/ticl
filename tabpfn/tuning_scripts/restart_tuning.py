bbb39f233c92403cbca19fd9a5d26a71ab251b23
amueller <amueller@microsoft.com>
2023-12-13 16:30:01 +0000

    restart synetune with more max failures
    Former-commit-id: 138c53257e4bcb336728c683a933199e749b8c6e

--- tabpfn/tuning_scripts/restart_tuning.py
+++ tabpfn/tuning_scripts/restart_tuning.py
@@ -5,7 +5,6 @@ root = logging.getLogger()
 root.setLevel(logging.DEBUG)
 
 tuner = load_experiment(sys.argv[1], load_tuner=True).tuner
-tuner.trial_backend.delete_checkpoints = True
-tuner.scheduler.early_checkpoint_removal_kwargs = {"max_num_checkpoints": 80}
-tuner.scheduler._initialize_early_checkpoint_removal({"max_num_checkpoints": 80})
+import pdb; pdb.set_trace()
+tuner.max_failures = 10000
 tuner.run()
