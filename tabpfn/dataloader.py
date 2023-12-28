ff5b1950e85586c7482f0ed83ece88bf401b0d57
Andreas Mueller <amueller@microsoft.com>
2023-12-27 16:55:53 -0800

    simplify
    Former-commit-id: 5131619c6e41f42bbfd7f11ed7b0fc4dd162e082

--- tabpfn/dataloader.py
+++ tabpfn/dataloader.py
@@ -2,31 +2,21 @@ import tabpfn.priors as priors
 from tabpfn.priors.flexible_categorical import FlexibleCategoricalPrior
 from tabpfn.priors.prior_bag import BagPrior
 
-def get_mlp_prior_hyperparameters(config):
-    config = {hp: (list(config[hp].values())[0]) if type(config[hp]) is dict else config[hp] for hp in config}
-    return config
-
-
-def get_gp_prior_hyperparameters(config):
-    return {hp: (list(config[hp].values())[0]) if type(config[hp]) is dict else config[hp] for hp in config}
-
-
 def get_dataloader(prior_type, config, steps_per_epoch, batch_size, single_eval_pos_gen, bptt, device):
     gp_flexible = FlexibleCategoricalPrior(priors.fast_gp.GPPrior())
     mlp_flexible = FlexibleCategoricalPrior(priors.mlp.MLPPrior())
     
     if prior_type == 'prior_bag':
         # Prior bag combines priors
-        prior_hyperparameters = {**get_mlp_prior_hyperparameters(config), **get_gp_prior_hyperparameters(config)}
+        hyperparameters = config.copy()
+        hyperparameters['num_features_used'] = config['num_features_used']['uniform_int_sampler_f(3,max_features)']
         bag_prior = BagPrior(base_priors={'gp': gp_flexible, 'mlp': mlp_flexible},
                              prior_exp_weights={'mlp': config['differentiable_hyperparameters']['prior_bag_exp_weights_1']}, verbose=True)
         extra_kwargs = {'get_batch': bag_prior.get_batch, 'differentiable_hyperparameters': config['differentiable_hyperparameters']}
-        #extra_kwargs = {'get_batch': priors.prior_bag.get_batch_bag, 'differentiable_hyperparameters': config['differentiable_hyperparameters']}
         DataLoader = priors.differentiable_prior.DataLoader
 
         extra_prior_kwargs_dict = {
-            # , 'dynamic_batch_size': 1 if ('num_global_att_tokens' in config and config['num_global_att_tokens']) else 2
-            'num_features': config['num_features'], 'hyperparameters': prior_hyperparameters, 'batch_size_per_gp_sample': config.get('batch_size_per_gp_sample', None), **extra_kwargs
+            'num_features': config['num_features'], 'hyperparameters': hyperparameters, 'batch_size_per_gp_sample': config.get('batch_size_per_gp_sample', None), **extra_kwargs
         }
     elif prior_type == "boolean_only":
         DataLoader = priors.boolean_conjunctions.DataLoader
