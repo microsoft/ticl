64ca82162d55d9e9fc82d8c028833561d854659c
Andreas Mueller <amueller@microsoft.com>
2024-01-23 21:29:01 -0800

    backward compat for boolean_prior config
    Former-commit-id: 15133468e5518845576aeb9dd8f0e16b868b8998

--- tabpfn/model_builder.py
+++ tabpfn/model_builder.py
@@ -108,6 +108,8 @@ def get_model(config, device, should_train=True, verbose=False, model_state=None
     config['adam_beta1'] = config.get('adam_beta1', 0.9)
     config['spike_tolerance'] = config.get('spike_tolerance', 4)
     config['min_lr'] = config.get('min_lr', None)
+    config['boolean_prior'] = config.get('boolean_prior', {})
+
     config['min_eval_pos'] = config.get('min_eval_pos', 2)
     config['stop_after_epochs'] = config.get('stop_after_epochs', None)
     config['low_rank_weights'] = config.get('low_rank_weights', config['weight_embedding_rank'] is not None)
