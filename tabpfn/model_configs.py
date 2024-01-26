d99ade38c6deac1f2389c77dcd415e11f631c0e6
Andreas Mueller <amueller@microsoft.com>
2024-01-26 13:08:33 -0800

    go back to old meta-distributions for comparison.
    Former-commit-id: 3f1a31dd275b45ef431c3e66dfb387740645a05b

--- tabpfn/model_configs.py
+++ tabpfn/model_configs.py
@@ -78,9 +78,12 @@ def get_diff_causal():
         "prior_mlp_hidden_dim": {'distribution': 'meta_gamma', 'max_alpha': 3, 'max_scale': 100, 'round': True, 'lower_bound': 4},
         "prior_mlp_dropout_prob": {'distribution': 'meta_beta', 'scale': 0.6, 'min': 0.1, 'max': 5.0},
         # This mustn't be too high since activations get too large otherwise
-        "init_std": {'distribution': 'log_uniform', 'min': 1e-2, 'max': 12},
-        "noise_std": {'distribution': 'log_uniform', 'min': 1e-4, 'max': .5},
-
+        #"init_std": {'distribution': 'log_uniform', 'min': 1e-2, 'max': 12},
+        #"noise_std": {'distribution': 'log_uniform', 'min': 1e-4, 'max': .5},
+        "noise_std": {'distribution': 'meta_trunc_norm_log_scaled', 'max_mean': .3, 'min_mean': 0.0001, 'round': False,
+                      'lower_bound': 0.0},
+        "init_std": {'distribution': 'meta_trunc_norm_log_scaled', 'max_mean': 10.0, 'min_mean': 0.01, 'round': False,
+                     'lower_bound': 0.0},
         "num_causes": {'distribution': 'meta_gamma', 'max_alpha': 3, 'max_scale': 7, 'round': True,
                        'lower_bound': 2},
 
