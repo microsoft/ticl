4766d50e2ab4237258ea546a08f38faccc2a42d5
Andreas Mueller <amueller@microsoft.com>
2024-02-07 18:57:28 -0800

    add mlp tests
    Former-commit-id: 2d8a72e2d895d87f7866b4af14663cb26b006092

---
+++ tabpfn/tests/priors/test_mlp_prior.py
@@ -0,0 +1,41 @@
+from tabpfn.priors import MLPPrior
+from tabpfn.model_configs import get_base_config
+import lightning as L
+import torch
+import pytest
+
+@pytest.mark.parametrize("num_features", [11, 51])
+@pytest.mark.parametrize("batch_size", [4, 8])
+@pytest.mark.parametrize("n_samples", [128, 900])
+def test_mlp_prior(batch_size, num_features, n_samples):
+    # test the mlp prior
+    L.seed_everything(42)
+    prior = MLPPrior()
+    hyperparameters = {
+        'pre_sample_causes': True,
+        'prior_mlp_activations': torch.nn.ReLU,
+        'is_causal' : False,
+        'num_causes': 3, # actually ignored because is_causal is False
+        'prior_mlp_hidden_dim': 128,
+        'num_layers': 3,
+        'noise_std': 0.1,
+        'y_is_effect': True,
+        'pre_sample_weights': False,
+        'prior_mlp_dropout_prob': 0.1,
+        'block_wise_dropout': True,
+        'init_std': 0.1,
+        'sort_features': False,
+        'in_clique': False,
+        'verbose': False,
+        'prior_mlp_scale_weights_sqrt': True,
+        'random_feature_rotation': True,
+        'add_uninformative_features': False,
+    }
+    x, y, y_ = prior.get_batch(batch_size=batch_size, num_features=num_features, n_samples=n_samples, device='cpu', hyperparameters=hyperparameters)
+    assert x.shape == (n_samples, batch_size, num_features)
+    assert y.shape == (n_samples, batch_size)
+    assert y_.shape == (n_samples, batch_size)
+    if n_samples == 128 and batch_size == 4 and num_features == 11:
+        assert float(x[0, 0, 0])== 1.0522834062576294
+        assert float(y[0, 0]) == -0.1148308664560318
+
