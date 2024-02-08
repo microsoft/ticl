8c0085bcda3ee824ebc2cfbe85b670ea1510f01e
Andreas Mueller <amueller@microsoft.com>
2024-02-07 19:28:57 -0800

    add test for sampling in classification prior
    Former-commit-id: 152bd56e84adeeef29541ffd3650a10a729f060b

--- tabpfn/tests/priors/test_classification_prior.py
+++ tabpfn/tests/priors/test_classification_prior.py
@@ -4,13 +4,13 @@ import torch
 import pytest
 
 from tabpfn.priors import ClassificationAdapterPrior, MLPPrior
-
+from tabpfn.priors.utils import uniform_int_sampler_f
 
 @pytest.mark.parametrize("num_features", [11, 51])
 @pytest.mark.parametrize("batch_size", [4, 8])
 @pytest.mark.parametrize("n_classes", [2, 4])
 @pytest.mark.parametrize("n_samples", [128, 900])
-def test_mlp_prior(batch_size, num_features, n_samples, n_classes):
+def test_classification_prior_no_sampling(batch_size, num_features, n_samples, n_classes):
     # test the mlp prior
     L.seed_everything(42)
     config = get_base_config()
@@ -42,4 +42,37 @@ def test_mlp_prior(batch_size, num_features, n_samples, n_classes):
     if n_samples == 128 and batch_size == 4 and num_features == 11 and n_classes == 2:
         assert float(x[0, 0, 0])== -0.46619218587875366
         assert float(y[0, 0]) == 1.0
+
+
+def test_classification_prior_with_sampling():
+    batch_size = 16
+    num_features = 100
+    n_samples = 900
+    n_classes = 8
+    # test the mlp prior
+    L.seed_everything(42)
+    config = get_base_config()
+    prior = ClassificationAdapterPrior(MLPPrior(config['prior']['mlp']), **config['prior']['classification'])
+    hyperparameters = {
+        'prior_mlp_activations': lambda: torch.nn.ReLU, # relu is callable so we'd try to call it thinking it's a sampling function....
+        'is_causal' : False,
+        'num_causes': 3, # actually ignored because is_causal is False
+        'prior_mlp_hidden_dim': 128,
+        'num_layers': uniform_int_sampler_f(1, 10),
+        'noise_std': 0.1,
+        'y_is_effect': True,
+        'pre_sample_weights': False,
+        'prior_mlp_dropout_prob': 0.1,
+        'block_wise_dropout': True,
+        'init_std': 0.1,
+        'sort_features': False,
+        'in_clique': False,
+    }
+    x, y, y_ = prior.get_batch(batch_size=batch_size, num_features=num_features, n_samples=n_samples, device='cpu', hyperparameters=hyperparameters)
+    assert x.shape == (n_samples, batch_size, num_features)
+    assert y.shape == (n_samples, batch_size)
+    assert y_.shape == (n_samples, batch_size)
+
+    assert float(x[0, 0, 0])== 0.21882277727127075
+    assert float(y[0, 0]) == 0.0
 
