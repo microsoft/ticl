78b6ce5912ac14b4d0ceefbbbd395bf6b6a54433
Andreas Mueller <amueller@microsoft.com>
2024-02-27 09:56:14 -0800

    more loading old config stuff

--- mothernet/model_builder.py
+++ mothernet/model_builder.py
@@ -96,10 +96,15 @@ def old_config_to_new(old_config, new_config):
         old_config['model_type'] = old_config.pop('model_maker')
     if "model_type" not in old_config:
         old_config['model_type'] = 'tabpfn'
+    if old_config.pop("special_token", False):
+        old_config['decoder_type'] = 'special_token'
+    if not old_config.pop("output_attention", True):
+        raise NotImplementedError("output_attention=False is not supported anymore")
     ignored_configs = ['seq_len_used', 'verbose', 'noise_type', 'normalize_to_ranking', 'normalize_by_used_features', 'num_categorical_features_sampler_a',
                        'differentiable', 'flexible', 'bptt_extra_samples', 'dynamic_batch_size', 'new_mlp_per_example', 'batch_size_per_gp_sample',
                        'normalize_ignore_label_too', 'differentiable_hps_as_style', 'rotate_normalized_labels', 'canonical_y_encoder',
-                       'total_available_time_in_s', 'normalize_with_sqrt', 'done_part_in_training', 'mix_activations']
+                       'total_available_time_in_s', 'normalize_with_sqrt', 'done_part_in_training', 'mix_activations',
+                       'perceiver_large_dataset', 'no_double_embedding', 'losses', 'wallclock_times', 'learning_rates', 'num_gpus', 'device']
     for k in ignored_configs:
         old_config.pop(k, None)
     for k, v in new_config.items():
