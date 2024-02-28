6c0337dd9f9e9d846c0d41418d7696bee21ac5b4
Andreas Mueller <amueller@microsoft.com>
2024-02-28 13:57:51 -0800

    add test for additive model from blob storage

--- mothernet/tests/test_follow_sklearn_interface.py
+++ mothernet/tests/test_follow_sklearn_interface.py
@@ -2,7 +2,7 @@ import pickle
 
 import numpy as np
 
-from mothernet.prediction import TabPFNClassifier, MotherNetClassifier
+from mothernet.prediction import TabPFNClassifier, MotherNetClassifier, MotherNetAdditiveClassifier
 from mothernet.evaluation.baselines.distill_mlp import DistilledTabPFNMLP
 from mothernet.utils import get_mn_model
 
@@ -63,6 +63,19 @@ def test_mothernet_jan_2024():
     model_string = "mn_P512_SFalse_L2_1_gpu_01_24_2024_00_31_59_epoch_3950.cpkt"
     model_path = get_mn_model(model_string)
     classifier = MotherNetClassifier(device='cpu', path=model_path)
+    classifier.fit(X_train, y_train)
+    print(classifier)
+    prob = classifier.predict_proba(X_test)
+    assert (prob.argmax(axis=1) == classifier.predict(X_test)).all()
+    assert classifier.score(X_test, y_test) > 0.9
+
+
+def test_additive_mothernet_dense():
+    X, y = load_iris(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+    model_string = "additive_1_gpu_02_14_2024_16_34_15_epoch_950_fixed2.cpkt"
+    model_path = get_mn_model(model_string)
+    classifier = MotherNetAdditiveClassifier(device='cpu', path=model_path)
     classifier.fit(X_train, y_train)
     print(classifier)
     prob = classifier.predict_proba(X_test)
