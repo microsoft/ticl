a9233a82371346a4537ab0245b0008f9f0d7ae33
juliensiems <juliensiems@gmail.com>
2024-04-01 13:51:14 +0100

    Change variable name in step function prior for readability. Sample step threshold directly making sure not to sample at the edges which would produce no step function. Add unit test checking that there is one step function per feature.

--- mothernet/priors/step_function_prior.py
+++ mothernet/priors/step_function_prior.py
@@ -19,28 +19,18 @@ class StepFunctionPrior:
                 x = torch.randn(batch_size, n_samples, num_features, device=device)
 
             # Per element in batch create a random step function per feature and add them across samples.
-            start = torch.randint(0, 2, size=(batch_size, num_features), device=device).unsqueeze(dim=1)
-            step = torch.randint(0, n_samples, (batch_size, num_features), device=device)
-
-            # Expand step to have an extra dimension
-            step = step[:, :, None].expand(-1, -1, n_samples)
-
-            # Create a tensor to hold the indices for the batch and feature dimensions
-            batch_indices = torch.arange(batch_size)[:, None, None].expand(-1, num_features, n_samples).to(device)
-            feature_indices = torch.arange(num_features)[None, :, None].expand(batch_size, -1, n_samples).to(device)
-
-            # Use these indices to gather the appropriate elements from x
-            x_gathered = x[batch_indices, step, feature_indices]
+            up_or_down = torch.randint(0, 2, size=(batch_size, num_features), device=device).unsqueeze(dim=1)
+            step = (x.max(dim=1).values - x.min(dim=1).values) * 0.8 * torch.rand((batch_size, num_features)) + x.min(dim=1).values
 
             # Compute the step function for the entire tensor at once
-            step_function = ((x < x_gathered.permute(0, 2, 1)).int() + start) % 2
+            step_function = ((x < step.unsqueeze(1)).int() + up_or_down) % 2
             y = (step_function.float().sum(dim=-1) > 0).float()
 
             x = x.permute(1, 0, 2)  # (n_samples, batch_size, num_features)
             y = y.permute(1, 0)  # (n_samples, batch_size)
-            return x, y, step_function
+            return x, y, step_function, step
 
     def get_batch(self, batch_size, n_samples, num_features, device=default_device, num_outputs=1, epoch=None,
                   single_eval_pos=None):
-        x, y, step_function = self._get_batch(batch_size, n_samples, num_features, device, num_outputs, epoch,)
+        x, y, _, _ = self._get_batch(batch_size, n_samples, num_features, device, num_outputs, epoch,)
         return x, y, y
