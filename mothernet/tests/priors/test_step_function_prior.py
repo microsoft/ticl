74fa404205e0cca286bc7e42813959a77dbacd33
juliensiems <juliensiems@gmail.com>
2024-04-01 21:01:53 +0100

    I added feature sparsity to the prior now it only uses up to 2 features in the step function and the other ones never contribute.

--- mothernet/tests/priors/test_step_function_prior.py
+++ mothernet/tests/priors/test_step_function_prior.py
@@ -11,17 +11,21 @@ from mothernet.priors.step_function_prior import StepFunctionPrior
 def test_step_function_prior(num_features, batch_size, n_samples):
     L.seed_everything(42)
     prior = StepFunctionPrior({'max_steps': 1, 'sampling': 'uniform'})
-    x, y, step_function, step = prior._get_batch(batch_size=batch_size, n_samples=n_samples,
-                                           num_features=num_features, device='cpu')
+    x, y, step_function, step, mask = prior._get_batch(batch_size=batch_size, n_samples=n_samples,
+                                                       num_features=num_features, device='cpu')
     assert x.shape == (n_samples, batch_size, num_features)
     assert y.shape == (n_samples, batch_size)
 
     # Iterate over each feature
     for batch_i in range(batch_size):
         for feature_i in range(step_function.shape[2]):
-            x_i, step_i = zip(*sorted(zip(x[:, batch_i, feature_i], step_function[batch_i, :, feature_i])))
-            step_i = torch.Tensor(step_i).long()
-            # Count the number of steps in the feature
-            steps = sum(step_i[1:] != step_i[:-1])
-            # Check that there is only a single step
-            assert steps == 1, f"Feature {feature_i} has {steps} steps"
+            if mask[batch_i, feature_i]:
+                # Check that the step function is zero for all samples
+                assert torch.all(step_function[batch_i, :, feature_i] == 0)
+            else:
+                x_i, step_i = zip(*sorted(zip(x[:, batch_i, feature_i], step_function[batch_i, :, feature_i])))
+                step_i = torch.Tensor(step_i).long()
+                # Count the number of steps in the feature
+                steps = sum(step_i[1:] != step_i[:-1])
+                # Check that there is only a single step
+                assert steps == 1, f"Feature {feature_i} has {steps} steps"
