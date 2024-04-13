c7fb44c62ed96b77f37273e169f1be5377e7f7bf
Andreas Mueller <amueller@microsoft.com>
2024-04-13 09:03:43 -0700

    add nograd to additive model evaluation

--- mothernet/prediction/mothernet_additive.py
+++ mothernet/prediction/mothernet_additive.py
@@ -14,77 +14,78 @@ from mothernet.utils import normalize_data
 def extract_additive_model(model, X_train, y_train, device="cpu", inference_device="cpu", pad_zeros=True):
     if "cuda" in inference_device and device == "cpu":
         raise ValueError("Cannot run inference on cuda when model is on cpu")
-    n_classes = len(np.unique(y_train))
-    n_features = X_train.shape[1]
+    with torch.no_grad():
+        n_classes = len(np.unique(y_train))
+        n_features = X_train.shape[1]
 
-    ys = torch.Tensor(y_train).to(device)
-    xs = torch.Tensor(X_train).to(device)
+        ys = torch.Tensor(y_train).to(device)
+        xs = torch.Tensor(X_train).to(device)
 
-    if X_train.shape[1] > 100:
-        raise ValueError("Cannot run inference on data with more than 100 features")
-    if pad_zeros:
-        x_all_torch = torch.concat([xs, torch.zeros((X_train.shape[0], 100 - X_train.shape[1]), device=device)], axis=1)
-    else:
-        x_all_torch = xs
-    X_onehot, bin_edges = bin_data(x_all_torch, n_bins=model.n_bins, nan_bin=model.nan_bin)
-    if model.input_layer_norm:
-        X_onehot = model.input_norm(X_onehot.float())
-    if getattr(model, "fourier_features", 0) > 0:
-        x_scaled = normalize_data(xs)
-        x_fourier = get_fourier_features(x_scaled, model.fourier_features)
-        X_onehot = torch.cat([X_onehot, x_fourier], -1)
-
-    x_src = model.encoder(X_onehot.unsqueeze(1).float())
-    if getattr(model, 'categorical_embedding', False):
-        is_categorical = model._determine_is_categorical(x_src)  # (1, batch_size, num_features)
-        x_src += model.is_categorical_encoder(is_categorical)
-
-    if model.y_encoder is None:
-        train_x = x_src
-    else:
-        y_src = model.y_encoder(ys.unsqueeze(1).unsqueeze(-1))
-        if x_src.ndim == 4:
-            # baam model, per feature
-            y_src = y_src.unsqueeze(-2)
-        train_x = x_src + y_src
-    assert train_x.shape == x_src.shape
-    if hasattr(model, "layers"):
-        # baam model
-        output = train_x
-        for mod in model.layers:
-            output = mod(output)
-    else:
-        output = model.transformer_encoder(train_x)
-
-    if model.marginal_residual in [True, 'True', 'output', 'decoder']:
-        class_averages = model.class_average_layer(X_onehot.float().unsqueeze(1), ys.unsqueeze(1))
-        # class averages are batch x outputs x features x bins
-        # output is batch x features x bins x outputs
-        marginals = model.marginal_residual_layer(class_averages)
-
-    if model.marginal_residual == 'decoder':
-        weights, biases = model.decoder(output, ys, marginals)
-    else:
-        weights, biases = model.decoder(output, ys)
-
-    if model.marginal_residual in [True, 'True', 'output', 'decoder']:
-        if hasattr(model, "layers") and len(model.layers) == 0:
-            weights = marginals.permute(0, 2, 3, 1)
+        if X_train.shape[1] > 100:
+            raise ValueError("Cannot run inference on data with more than 100 features")
+        if pad_zeros:
+            x_all_torch = torch.concat([xs, torch.zeros((X_train.shape[0], 100 - X_train.shape[1]), device=device)], axis=1)
         else:
-            weights = weights + marginals.permute(0, 2, 3, 1)
-    w = weights.squeeze()[:n_features, :, :n_classes]
-    if biases is None:
-        b = torch.zeros(n_classes, device=device)
-    else:
-        b = biases.squeeze()[:n_classes]
-    bins_data_space = bin_edges[:n_features]
-    # remove extra classes on output layer
-    if inference_device == "cpu":
-        def detach(x):
-            return x.detach().cpu().numpy()
-    else:
-        def detach(x):
-            return x.detach()
+            x_all_torch = xs
+        X_onehot, bin_edges = bin_data(x_all_torch, n_bins=model.n_bins, nan_bin=model.nan_bin)
+        if model.input_layer_norm:
+            X_onehot = model.input_norm(X_onehot.float())
+        if getattr(model, "fourier_features", 0) > 0:
+            x_scaled = normalize_data(xs)
+            x_fourier = get_fourier_features(x_scaled, model.fourier_features)
+            X_onehot = torch.cat([X_onehot, x_fourier], -1)
+
+        x_src = model.encoder(X_onehot.unsqueeze(1).float())
+        if getattr(model, 'categorical_embedding', False):
+            is_categorical = model._determine_is_categorical(x_src)  # (1, batch_size, num_features)
+            x_src += model.is_categorical_encoder(is_categorical)
+
+        if model.y_encoder is None:
+            train_x = x_src
+        else:
+            y_src = model.y_encoder(ys.unsqueeze(1).unsqueeze(-1))
+            if x_src.ndim == 4:
+                # baam model, per feature
+                y_src = y_src.unsqueeze(-2)
+            train_x = x_src + y_src
+        assert train_x.shape == x_src.shape
+        if hasattr(model, "layers"):
+            # baam model
+            output = train_x
+            for mod in model.layers:
+                output = mod(output)
+        else:
+            output = model.transformer_encoder(train_x)
+
+        if model.marginal_residual in [True, 'True', 'output', 'decoder']:
+            class_averages = model.class_average_layer(X_onehot.float().unsqueeze(1), ys.unsqueeze(1))
+            # class averages are batch x outputs x features x bins
+            # output is batch x features x bins x outputs
+            marginals = model.marginal_residual_layer(class_averages)
+
+        if model.marginal_residual == 'decoder':
+            weights, biases = model.decoder(output, ys, marginals)
+        else:
+            weights, biases = model.decoder(output, ys)
+
+        if model.marginal_residual in [True, 'True', 'output', 'decoder']:
+            if hasattr(model, "layers") and len(model.layers) == 0:
+                weights = marginals.permute(0, 2, 3, 1)
+            else:
+                weights = weights + marginals.permute(0, 2, 3, 1)
+        w = weights.squeeze()[:n_features, :, :n_classes]
+        if biases is None:
+            b = torch.zeros(n_classes, device=device)
+        else:
+            b = biases.squeeze()[:n_classes]
+        bins_data_space = bin_edges[:n_features]
+        # remove extra classes on output layer
+        if inference_device == "cpu":
+            def detach(x):
+                return x.detach().cpu().numpy()
+        else:
+            def detach(x):
+                return x.detach()
 
     return detach(w), detach(b), detach(bins_data_space)
 
