e42d864540275d4ae85a4fd4404462d4a8563ab8
Andreas Mueller <amueller@microsoft.com>
2024-04-17 10:01:25 -0700

    missed some changes in shape function plotting.

--- mothernet/evaluation/plot_shape_function.py
+++ mothernet/evaluation/plot_shape_function.py
@@ -2,31 +2,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_shape_function(bin_edges: np.ndarray, w: np.ndarray, feature_names=None):
+def plot_shape_function(bin_edges: np.ndarray, w: np.ndarray, feature_names=None, feature_subset=None):
     num_classes = w.shape[2]
-    num_features = len(bin_edges)
+    num_features = len(feature_subset) if feature_subset is not None else len(bin_edges)
     if num_classes > 2:
         class_range = range(num_classes)
         rows, columns = num_classes, num_features
     else:
-        class_range = [0]
-        columns = min(int(np.sqrt(num_features)), 6)
+        class_range = [1]
+        columns = min(int(np.ceil(np.sqrt(num_features))), 6)
         rows = int(np.ceil(num_features / columns))
-    fig, axs = plt.subplots(rows, columns, figsize=(2*rows, 2*columns),
+    fig, axs = plt.subplots(rows, columns, figsize=(4*columns, 2*rows),
                             sharey=True)
+    feature_range = feature_subset if feature_subset is not None else range(num_features)
     for class_idx in class_range:
-        for feature_idx in range(num_features):
+        for ax_idx, feature_idx in enumerate(feature_range):
             weights_normalized = w[feature_idx][0:-1][:, class_idx] - w[feature_idx][0:-1].mean(axis=-1)
             if num_classes > 2:
-                ax = axs[class_idx][feature_idx]
+                ax = axs[class_idx][ax_idx]
             else:
-                ax = axs.ravel()[feature_idx]
+                ax = axs.ravel()[ax_idx]
             ax.step(bin_edges[feature_idx], weights_normalized)
-            if class_idx == 0:
+            if class_idx == 0 or num_classes == 2:
                 if feature_names is None:
                     ax.set_title(f'Feature {feature_idx}')
                 else:
-                    ax.set_title(feature_names[feature_idx])
+                    ax.set_title(f'{feature_idx}: {feature_names[feature_idx]}')
 
             if feature_idx == 0:
                 ax.set_ylabel(f'Class {class_idx}')
@@ -34,4 +35,4 @@ def plot_shape_function(bin_edges: np.ndarray, w: np.ndarray, feature_names=None
         for i in range(num_features, len(axs.ravel())):
             axs.ravel()[i].set_axis_off()
     plt.tight_layout()
-    plt.show()
+    return axs
