b2b24fd27c4af437ed07cd719bb0e5b43ea16788
Andreas Mueller <amueller@microsoft.com>
2024-04-20 13:39:50 -0700

    refactor everything lol

--- mothernet/evaluation/demo_app.py
+++ mothernet/evaluation/demo_app.py
@@ -21,7 +21,7 @@ from sklearn.metrics import roc_auc_score
 torch.set_num_threads(1)
 from sklearn.pipeline import make_pipeline
 import numpy as np
-from bokeh.models import Div
+from bokeh.models import Div, CheckboxButtonGroup
 
 from mothernet.utils import get_mn_model
 
@@ -73,80 +73,29 @@ for col in X_train.columns:
         drop_cols.append(col)
 X_train = X_train.drop(columns=drop_cols)
 
-
-device = "cpu"
-#additive = MotherNetAdditiveClassifier(path="../models_diff/baam_H512_Dclass_average_e128_nsamples500_numfeatures20_padzerosFalse_03_14_2024_15_03_22_epoch_1520.cpkt", device=device)
-ct = make_column_transformer((OneHotEncoder(sparse_output=False, max_categories=10, handle_unknown='ignore'), X_train.dtypes == object), remainder="passthrough", verbose_feature_names_out=False)
-
-model_string = "baam_H512_Dclass_average_e128_nsamples500_numfeatures20_padzerosFalse_03_14_2024_15_03_22_epoch_1520.cpkt"
-model_path = get_mn_model(model_string)
-
-additive = MotherNetAdditiveClassifier(path=model_path, device="cpu")
-pipe = make_pipeline(ct, VarianceThreshold(), additive)
-
-
 cat_cols = X_train.dtypes.index[X_train.dtypes == "object"]
 cont_cols = X_train.dtypes.index[X_train.dtypes != "object"]
 
-subsample = np.random.permutation(X_train.shape[0])[:100]
 
-pipe.fit(X_train.iloc[subsample], y_train.iloc[subsample])
-feature_names = pipe[:-1].get_feature_names_out()
-selected_features = [list(feature_names).index(col) for col in cont_cols if col in feature_names]
-additive = pipe[-1]
+def fit_predict_gamma_net(filter_feature=None, filter_value=None):
+    device = "cpu"
+    #additive = MotherNetAdditiveClassifier(path="../models_diff/baam_H512_Dclass_average_e128_nsamples500_numfeatures20_padzerosFalse_03_14_2024_15_03_22_epoch_1520.cpkt", device=device)
+    ct = make_column_transformer((OneHotEncoder(sparse_output=False, max_categories=10, handle_unknown='ignore'), X_train.dtypes == object), remainder="passthrough", verbose_feature_names_out=False)
 
+    model_string = "baam_H512_Dclass_average_e128_nsamples500_numfeatures20_padzerosFalse_03_14_2024_15_03_22_epoch_1520.cpkt"
+    model_path = get_mn_model(model_string)
 
-axes = plot_shape_function(additive.bin_edges_, additive.w_, feature_names=feature_names, feature_subset=selected_features)
+    additive = MotherNetAdditiveClassifier(path=model_path, device="cpu")
+    pipe = make_pipeline(ct, VarianceThreshold(), additive)
 
-select = ["None"] + list(cat_cols)
-per_col_cats = X_train[cat_cols].apply(lambda x: pd.unique(x).tolist()).to_dict()
-
-
-menu = [(s, s) for s in select]
-
-cats = Dropdown(label="None", menu=menu, width=100)
-vals = Dropdown(label="None", menu=[("None", "None")], width=100)
-
-slice_label = Div(text="Slice By", margin=(10, 2, 0, 20))
-value_label = Div(text="Value", margin=(10, 2, 0, 20))
-some_output = Div(text="", margin=(10, 2, 0, 20))
-
-
-def pick_feature(event):
-    print("callback")
-    print(event)
-    selection = event.item
-    print(selection)
-    cats.label = selection
-    if selection != "None":
-        old_vals = vals.label
-        new_menu = [("None", "None")] + [(str(v), str(v)) for v in per_col_cats[selection]]
-        print(vals.menu)
-        vals.update(label="None", menu= new_menu)
-        vals.label = "bla"
-        vals.label = "None"
-        if old_vals != "None":
-            plot_with_val(value_item="None")
-    else:
-        vals.menu = [("None", "None")]
-        vals.label = "None"
-        plot_with_val(value_item="None")
-
-def select_val(event):
-    print("select val callback")
-    print(event)
-    value_item = event.item
-    print(f"selected value: {value_item}")
-    plot_with_val(value_item)
-
-def plot_with_val(value_item):
+    subsample = np.random.permutation(X_train.shape[0])[:100]
     vals.label = value_item
-    if value_item == "None":
+    if filter_value == "None":
         mask = np.ones(X_train.shape[0], dtype="bool")
         mask_test = np.ones(X_test.shape[0], dtype="bool")
     else:
-        mask = X_train[cats.label] == value_item
-        mask_test = X_test[cats.label] == value_item
+        mask = X_train[filter_feature] == value_item
+        mask_test = X_test[filter_feature] == value_item
     some_output.text = "fitting..."
     X_train_masked = X_train[mask]
     y_train_masked = y_train[mask]
@@ -171,19 +120,99 @@ def plot_with_val(value_item):
     feature_names = pipe[:-1].get_feature_names_out()
     selected_features = [list(feature_names).index(col) for col in cont_cols if col in feature_names]
     additive = pipe[-1]
-    for bins, w, feature in zip(additive.bin_edges_, additive.w_, feature_names):
+    return pipe, additive.bin_edges_, additive.w_, feature_names, selected_features
+
+# axes = plot_shape_function(additive.bin_edges_, additive.w_, feature_names=feature_names, feature_subset=selected_features)
+
+select = ["None"] + list(cat_cols)
+per_col_cats = X_train[cat_cols].apply(lambda x: pd.unique(x).tolist()).to_dict()
+
+
+menu = [(s, s) for s in select]
+
+cats = Dropdown(label="None", menu=menu, width=100)
+vals = Dropdown(label="None", menu=[("None", "None")], width=100)
+
+slice_label = Div(text="Slice By", margin=(10, 2, 0, 20))
+value_label = Div(text="Value", margin=(10, 2, 0, 20))
+some_output = Div(text="", margin=(10, 2, 0, 20))
+
+checkbox_button_group = CheckboxButtonGroup(labels=['EBM', 'GammaNet'], active=[])
+
+
+def pick_feature(event):
+    print("callback")
+    print(event)
+    selection = event.item
+    print(selection)
+    cats.label = selection
+    if selection != "None":
+        old_vals = vals.label
+        new_menu = [("None", "None")] + [(str(v), str(v)) for v in per_col_cats[selection]]
+        print(vals.menu)
+        vals.update(label="None", menu= new_menu)
+        vals.label = "bla"
+        vals.label = "None"
+        if old_vals != "None":
+            fit_predict_with_model()
+    else:
+        vals.menu = [("None", "None")]
+        vals.label = "None"
+        fit_predict_with_model()
+
+def select_val(event):
+    print("select val callback")
+    print(event)
+    value_item = event.item
+    print(f"selected value: {value_item}")
+    fit_predict_with_model()
+
+def update_plot(bin_edges, w, feature_names):
+    for bins, w, feature in zip(bin_edges, w, feature_names):
         if feature not in grid_figures:
             continue
         p, my_step = grid_figures[feature]
         weights_normalized = w[0:-1][:, 1] - w[0:-1].mean(axis=-1)
         my_step.data_source.data['y'] = weights_normalized
         my_step.data_source.data['x'] = bins
-    #axes = plot_shape_function(additive.bin_edges_, additive.w_, feature_names=feature_names, feature_subset=selected_features)
+
+def get_model():
+    if 0 in checkbox_button_group.active:
+        return "ebm"
+    if 1 in checkbox_button_group.active:
+        return "gamma_net"
+    return None
+
+def fit_predict_with_model():
+    model = get_model()
+    if model == "ebm":
+        print("fitting ebm")
+        return
+    if model == "gamma_net":
+        print("fitting gamma_net")
+        pipe, bin_edges, w, feature_names, selected_features = fit_predict_gamma_net(cats.label, vals.label)
+
+    if len(grid_figures) == 0:
+        plot_shape_function(bin_edges, w, feature_names=feature_names, feature_subset=selected_features)
+    else:
+        update_plot(bin_edges, w, feature_names)
+    
+
+
+def checkbox_event(attr, old, new):
+    if len(new) == 2:
+        new_val = [x for x in new if x not in old]
+        print(f"setting to {new_val}")
+        checkbox_button_group.active = new_val
+    if new_val != old[0]:
+        fit_predict_with_model()
+                
 
 #cats.on_change('label', my_func)
 cats.on_click(pick_feature)
 vals.on_click(select_val)
-col = layout([[slice_label, cats, value_label, vals, some_output], [axes]])
+checkbox_button_group.on_change("active", checkbox_event)
+col = layout([[checkbox_button_group, slice_label, cats, value_label, vals, some_output], [axes]])
 print("prestart")
 curdoc().add_root(col)
 print("starting")
