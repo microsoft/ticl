d65499977a4ccf30e997cec11e4f94591de1ad46
juliensiems <juliensiems@gmail.com>
2024-05-02 10:29:57 +0200

    Add sklearn like binning also for nan bins.

--- mothernet/models/utils.py
+++ mothernet/models/utils.py
@@ -3,6 +3,24 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+def sklearn_like_binning(bin_edges: torch.Tensor, n_bins: int, batch_size: int, num_features: int, data_nona):
+    # Bin like in sklearn if there are not enough unique values
+    # https://github.com/scikit-learn/scikit-learn/blob/3ee60a720aab3598668af3a3d7eb01d6958859be/sklearn/ensemble/_hist_gradient_boosting/binning.py#L53
+    for batch_idx in range(batch_size):
+        for col_idx in range(num_features):
+            unique_vals = data_nona[col_idx, batch_idx, :].unique(sorted=True).flatten()
+            if len(unique_vals) == 1:
+                # If all values are the same, we can't bin
+                bin_edges[col_idx, batch_idx] = unique_vals
+            elif len(unique_vals) < n_bins:
+                bin_edges_cat = (unique_vals[:-1] + unique_vals[1:]) * 0.5
+                bin_edges_cat = F.interpolate(bin_edges_cat.unsqueeze(0).unsqueeze(0),
+                                              size=n_bins - 1, mode='nearest')
+                bin_edges[col_idx, batch_idx] = bin_edges_cat.squeeze()
+            else:
+                pass
+
+
 def bin_data(data, n_bins, nan_bin=False, single_eval_pos=None, sklearn_binning: bool = False):
     if nan_bin:
         # data is samples x batch x features
@@ -22,6 +40,9 @@ def bin_data(data, n_bins, nan_bin=False, single_eval_pos=None, sklearn_binning:
 
         # Fill NaNs in order to bin the data.
         data = torch.nan_to_num(data, nan=0.0)
+        batch_size, num_features = data.shape[1], data.shape[0]
+        if sklearn_binning:
+            sklearn_like_binning(bin_edges, n_bins - 1, batch_size, num_features, data)
         X_binned = torch.searchsorted(bin_edges, data)
 
         # Put NaN data on the last bin.
@@ -40,23 +61,10 @@ def bin_data(data, n_bins, nan_bin=False, single_eval_pos=None, sklearn_binning:
         bin_edges = bin_edges.transpose(0, -1).contiguous()
         data_nona = data_nona.transpose(0, -1).contiguous()
 
-        # Bin like in sklearn if there are not enough unique values
-        # https://github.com/scikit-learn/scikit-learn/blob/3ee60a720aab3598668af3a3d7eb01d6958859be/sklearn/ensemble/_hist_gradient_boosting/binning.py#L53
+        batch_size, num_features = data_nona.shape[1], data_nona.shape[0]
+
         if sklearn_binning:
-            # Vanilla binning
-            for batch_idx in range(data_nona.shape[1]):
-                for col_idx in range(data_nona.shape[0]):
-                    unique_vals = data_nona[col_idx, batch_idx, :].unique(sorted=True).flatten()
-                    if len(unique_vals) == 1:
-                        # If all values are the same, we can't bin
-                        bin_edges[col_idx, batch_idx] = unique_vals
-                    elif len(unique_vals) < n_bins:
-                        bin_edges_cat = (unique_vals[:-1] + unique_vals[1:]) * 0.5
-                        bin_edges_cat = F.interpolate(bin_edges_cat.unsqueeze(0).unsqueeze(0),
-                                                      size=n_bins - 1, mode='nearest')
-                        bin_edges[col_idx, batch_idx] = bin_edges_cat.squeeze()
-                    else:
-                        pass
+            sklearn_like_binning(bin_edges, n_bins, batch_size, num_features, data_nona)
 
         X_binned = torch.searchsorted(bin_edges, data_nona)
         X_onehot = nn.functional.one_hot(X_binned.transpose(0, -1), num_classes=n_bins)
